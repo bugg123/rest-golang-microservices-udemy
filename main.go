be5b8824753533764826b71c55cb1b35190f60e1
Corey Hinkle <corey.hinkle@five9.com>
2021-05-24 18:52:26 -0400

    json marshalling

--- main.go
+++ main.go
@@ -1,19 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string `json:"name"`
+	City    string `json:"city"`
+	ZipCode string `json:"zip_code"`
+}
+
 func main() {
 
 	// define routes
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	// starting server
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello world!!!")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Corey", "Morgantown", "12345"},
+		{"Caleb", "Portland", "12345"},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customers)
 }
