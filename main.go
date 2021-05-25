0cadc073e320a7c7ed4eaff0510ba2adeb5611c3
Corey Hinkle <corey.hinkle@five9.com>
2021-05-25 10:50:33 -0400

    Fix typo

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func getAllCustomers(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "application/xml")
 		xml.NewEncoder(w).Encode(customers)
 	} else {
-		w.Header().Add("Content-Type", "application/xml")
+		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(customers)
 	}
 
